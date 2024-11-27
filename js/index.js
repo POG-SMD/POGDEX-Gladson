8d5d02536f1bdd20812c09b617f925630e35def2
gmpf123 <gladsonfilho123123@gmail.com>
2024-11-27 13:45:49 -0300

    feat: button to random pokemon and css

--- js/index.js
+++ js/index.js
@@ -26,3 +26,28 @@ async function setPokemonImg(num) {
 }
 
 for (let i = 1  ; i < 152; i++) setPokemonImg(i)
+
+    document.getElementById('random').addEventListener('click', async () => {
+        const overlay = document.getElementById('overlay');
+        const randomNum = Math.floor(Math.random() * 151) + 1;
+        const pokemon = await gerarPokemon(randomNum);
+
+        overlay.innerHTML = `
+        <article>
+            <h2>${pokemon.name}</h2>
+            <img src="${pokemon.sprite}" alt="${pokemon.name}">
+            <div>
+                <p class="type">${pokemon.type1}</p>
+                ${pokemon.type2 ? `<p class="type">${pokemon.type2}</p>` : ''}
+            </div>
+            <button id="close">Fechar</button>
+        </article>
+        `;
+    
+        overlay.classList.remove('hidden');
+
+        document.getElementById('close').addEventListener('click', () => {
+            overlay.classList.add('hidden');
+        });
+    });
+    
